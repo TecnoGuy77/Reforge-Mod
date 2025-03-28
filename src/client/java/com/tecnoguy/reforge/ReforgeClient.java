baa838ba411ea35010be3ab77f2f739d8b3c5a66
TecnoGuy77 <138028703+tecnoguy0@users.noreply.github.com>
2025-03-28 14:06:19 +0530

    first commit

---
+++ src/client/java/com/tecnoguy/reforge/ReforgeClient.java
@@ -0,0 +1,10 @@
+package com.tecnoguy.reforge;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReforgeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
